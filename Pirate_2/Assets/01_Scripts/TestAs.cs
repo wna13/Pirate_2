2d8c94a4d979a02c9e4c8eaa627fc25b07a20853
Jungwan Choi <jungwanchoi@Jungwanui-MacBookPro.local>
2025-09-12 22:13:17 +0900

    asset

---
+++ Pirate_2/Assets/01_Scripts/TestAs.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.AdaptivePerformance.Provider;
+
+public class TestAs : MonoBehaviour
+{
+    public GameObject aks;
+    [SerializeField] Sprite aksksk;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
